9199f4133e624ff38edfc32716048872a55bedab
akrollno21 <167443425+akrollno21@users.noreply.github.com>
2024-04-23 15:40:31 +0530

    Add files via upload

---
+++ पथ/src/temples.js
@@ -0,0 +1,63 @@
+
+let geojsonFeatureCollection = {
+
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "Temple No.": "1",
+                "Name": "Shri Krishna Janmasthan",
+                "City": "Mathura",
+                "State": "UP",
+                "Country": "Bharat",
+                "Status": "Open",
+                "Direction URL": "https://www.google.com/maps/dir/Shri+Krishna+Janmasthan,+Janam+bhoomi+Marg,+Janam+Bhumi,+Mathura,+Uttar+Pradesh+281001/27.5058468,77.6696013/@27.5470556,77.5523718,11.66z/data=!4m9!4m8!1m5!1m1!1s0x397371719a7a6a6f:0x79b85cb6bed8db11!2m2!1d77.669774!2d27.5049475!1m0!3e0?entry=ttu",
+                "popupContent": "Shri Krishna Janmasthan"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [77.6697917081457, 27.505111245661592]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Temple No.": "2",
+                "Name": "Shree Ram Janmabhoomi Mandir",
+                "City": "Ayodhya",
+                "State": "UP",
+                "Country": "Bharat",
+                "Status": "Open",
+                "Direction URL": "https://www.google.com/maps/dir//Shree+Ram+Janmabhoomi+Mandir,+Sai+Nagar,+Ayodhya,+Uttar+Pradesh+224123/@26.795688,82.1849038,16z/data=!4m9!4m8!1m0!1m5!1m1!1s0x399a07cd3079f0bf:0x5589e823b23ba9dc!2m2!1d82.194431!2d26.795688!3e0?entry=ttu",
+                "popupContent": "Shree Ram Janmabhoomi Mandir"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [82.19432371122066, 26.79622429789479]
+            }
+        },
+
+        {
+            "type": "Feature",
+            "properties": {
+                "Temple No.": "3",
+                "Name": "Shri Kashi Vishwanath Temple",
+                "City": "kashi",
+                "State": "UP",
+                "Country": "Bharat",
+                "Status": "Open",
+                "Direction URL": "https://www.google.com/maps/dir//Shri+Kashi+Vishwanath+Temple,+Lahori+Tola,+Varanasi,+Domari,+Uttar+Pradesh+221001/@26.2398989,81.6587713,8.65z/data=!4m9!4m8!1m0!1m5!1m1!1s0x398e2e21ee940b17:0x48600e4fcdfb7b13!2m2!1d83.0106783!2d25.3108532!3e0?entry=ttu",
+                "popupContent": "Shri Kashi Vishwanath Temple"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [83.00297390130713, 25.3387146092105]
+            }
+        }
+
+
+        // Add more store objects here
+    ]
+};
+
